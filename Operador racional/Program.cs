a0ad9a628b38c68403ea1e5ad704c947fbbae2a4
arman <arman@ARMANDODEJESUS>
2025-09-10 10:38:42 -0500

    Agregar archivos de proyecto.

---
+++ Operador racional/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+var (valor1, valor2, valor3) = (10,6,8);
+Console.WriteLine("los valores son:" + valor1+"  " + valor2+"  " + valor3);
+
+
+bool resultado1 = valor1 == valor2;
+Console.WriteLine("El resultado de la comparacion valor1 == valor2:" + resultado1);
+
+
+bool resultado2 = valor1 != valor2;
+Console.WriteLine("El resultado de la comparacion valor1 != valor2:" + resultado2);
+
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("El resultado de la comparacion valor1 > valor2:" + resultado3);
+
+
+bool resultado4 = valor1 < valor2;
+Console.WriteLine("El resultado de la comparacion valor1 < valor2:" + resultado4);
+
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("El resultado de la comparacion valor1 >= valor2:" + resultado5);
+
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("El resultado de la comparacion valor1 <= valor2:" + resultado6);
+
+
+
+
+
+
+
+//Igual(==) Devuelve verdadero si los operandos son iguales.
+//Diferente(!=)  Devuelve verdadero si los operandos son diferentes.
+//Mayor que(>)Devuelve verdadero si el operando de la izquierda es mayor que el de la derecha.
+//Menor que(<)Devuelve verdadero si el operando de la izquierda es menor que el de la derecha.
+//Mayor o igual que (>=)Devuelve verdadero si el operando de la izquierda es mayor o igual que el de la derecha.
+//Menor o igual que (<=)Devuelve verdadero si el operando de la izquierda es menor o igual que el de la derecha.
